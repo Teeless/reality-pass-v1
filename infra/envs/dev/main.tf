f7043a00cf94932c09fccc91c2f0db346add7680
Michael Hundelt <michael.hundelt@gmail.com>
2026-03-24 13:25:57 -0500

    Add DynamoDB tables for challenges and results

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -1,3 +1,25 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_dynamodb_table" "challenges" {
+  name         = "reality-pass-challenges-dev"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "challenge_id"
+
+  attribute {
+    name = "challenge_id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "results" {
+  name         = "reality-pass-results-dev"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "pass_id"
+
+  attribute {
+    name = "pass_id"
+    type = "S"
+  }
+}
