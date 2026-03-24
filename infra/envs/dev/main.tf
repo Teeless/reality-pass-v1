41ddef92d7d8e9267284b0d2ff99256b9b6b5c7c
Michael Hundelt <michael.hundelt@gmail.com>
2026-03-24 13:32:49 -0500

    Add AWS resources for Lambda and API Gateway

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -23,3 +23,50 @@ resource "aws_dynamodb_table" "results" {
     type = "S"
   }
 }
+resource "aws_iam_role" "lambda_exec" {
+  name = "reality-pass-lambda-role-dev"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_lambda_function" "verifier" {
+  function_name = "reality-pass-verifier-dev"
+  handler       = "handler.lambda_handler"
+  runtime       = "python3.11"
+  role          = aws_iam_role.lambda_exec.arn
+
+  filename         = "${path.module}/../../services/verifier/app.zip"
+  source_code_hash = filebase64sha256("${path.module}/../../services/verifier/app.zip")
+}
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "reality-pass-api-dev"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.verifier.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "verify" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /verify"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
+  auto_deploy = true
+}
